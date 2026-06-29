41e539536e38ef216f319b88317b8b190a867cf7
jovankawilyam <jovankamuzaki1@gmail.com>
2026-06-29 12:09:20 +0700

    update

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,19 +1,31 @@
+import { useSidebar } from "@/components/ui/sidebar"
+
 export default function AppLogo() {
-    return (
-        <>
-            <div className="flex aspect-square size-14 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <img
-                    src="/images/NAGARA-DANA-RAKCA.png"
-                    alt="kpknl-bogor"
-                    className="h-10 w-10"
-                />
-            </div>
-            <div className="grid flex-1 text-left text-sm">
-                <span className="mb-0.1 truncate leading-tight font-semibold">
-                    Doclang Boba
-                </span>
-                <span className="font-light">KPKNL Bogor</span>
-            </div>
-        </>
-    );
+  const { state } = useSidebar()
+  const isCollapsed = state === "collapsed"
+
+  return (
+    <>
+      <div className={`flex shrink-0 items-center justify-center transition-all duration-300 ${isCollapsed ? 'hidden' : 'aspect-square size-14 rounded-lg bg-sidebar-primary text-sidebar-primary-foreground flex items-center justify-center'}`}>
+        <img
+          src="/images/NAGARA-DANA-RAKCA.png"
+          alt="kpknl-bogor"
+          className="h-10 w-10"
+        />
+      </div>
+
+      <img
+          src="/images/NAGARA-DANA-RAKCA.png"
+          alt="kpknl-bogor"
+          className={`h-10 w-10 object-contain transition-all duration-300 ${isCollapsed ? 'block mx-auto' : 'hidden'}`}
+      />
+
+      <div className={`grid flex-1 text-left text-sm transition-all duration-300 ${isCollapsed ? 'opacity-0 w-0 h-0 hidden' : 'opacity-100'}`}>
+        <span className="mb-0.1 truncate leading-tight font-semibold">
+          Doclang Boba
+        </span>
+        <span className="font-light">KPKNL Bogor</span>
+      </div>
+    </>
+  );
 }
--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -56,16 +56,16 @@ const middleNavItems: NavItem[] = [
 ];
 
 const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
+    // {
+    //     title: 'Repository',
+    //     href: 'https://github.com/laravel/react-starter-kit',
+    //     icon: FolderGit2,
+    // },
+    // {
+    //     title: 'Documentation',
+    //     href: 'https://laravel.com/docs/starter-kits#react',
+    //     icon: BookOpen,
+    // },
 ];
 
 export function AppSidebar() {
